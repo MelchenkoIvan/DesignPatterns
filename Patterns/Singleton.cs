2b83ca39d88c065e4bb0fd6634b0f7b28cdb56c0
ivanmelchenko <imelchenko61@gmail.com>
2023-08-25 17:00:25 +0100

    feat(singleton): was created and tested pattern singleton

---
+++ Patterns/Singleton.cs
@@ -0,0 +1,10 @@
+namespace Patterns;
+
+public sealed class Singleton
+{
+    private Singleton() { }
+    private static readonly Lazy<Singleton> Instance = new Lazy<Singleton>(() => new Singleton());
+
+    public static Singleton GetInstance() => Instance.Value;
+    
+}
